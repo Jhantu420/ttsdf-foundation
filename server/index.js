ceb5e10cf730974fb627406dc664eac817d0daef
Jhantu Samui <106798967+Jhantu420@users.noreply.github.com>
2025-03-27 00:43:04 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -11,7 +11,7 @@ dotenv.config();
 
 const PORT = process.env.PORT || 4000;
 const app = express();
-const allowedOrigins = ["http://localhost:5173"];
+const allowedOrigins = ["https://ttsdf-foundation-client.onrender.com"];
 
 app.use(
   cors({
