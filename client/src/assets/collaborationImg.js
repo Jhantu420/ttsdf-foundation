fc41f7288dee2a691831085fb624bcee408774e3
Jhantu Samui <106798967+Jhantu420@users.noreply.github.com>
2025-04-01 00:15:18 +0530

    Add files via upload

---
+++ client/src/assets/collaborationImg.js
@@ -0,0 +1,37 @@
+import image1 from "../assets/brainware_university.png";
+import image2 from "../assets/neotia_univ.png";
+import image3 from "../assets/ADAMAS_Logo.png";
+import image4 from "../assets/JIS_group.png";
+import image5 from "../assets/Techno_india.jpg";
+import image6 from "../assets/Calcata University.jpeg";
+import image7 from "../assets/Secom_Eng.jpeg";
+import image8 from "../assets/OmDayal Group.jpg";
+import image9 from "../assets/Ambuja_Foundation.jpeg";
+import image10 from "../assets/SKIPPER.png";
+import image11 from "../assets/flipcart.png";
+import image12 from "../assets/Amazon.png";
+import image13 from "../assets/Bajaj-logo.jpg";
+import image14 from "../assets/Tata.jpeg";
+import image15 from "../assets/cropped-Bhandari-Automobiles.png";
+import image16 from "../assets/Reliance.png";
+import image17 from "../assets/Birla Cement.jpeg";
+const images = [
+  { src: image1, name: "Brainware University" },
+  { src: image2, name: "The Neotia University" },
+  { src: image3, name: "Adamas University" },
+  { src: image4, name: "JIS group" },
+  { src: image5, name: "Techno India Universety" },
+  { src: image6, name: "Calcuta University" },
+  { src: image7, name: "Seacom Engineering Collage" },
+  { src: image8, name: "OmDayal Group" },
+  { src: image9, name: "Ambuja Foundation" },
+  { src: image10, name: "Skipper Limited" },
+  { src: image11, name: "Flipkart" },
+  { src: image12, name: "Amazon" },
+  { src: image13, name: "Bajaj" },
+  { src: image14, name: "Tata" },
+  { src: image15, name: "Bhandari-Automobiles" },
+  { src: image16, name: "Reliance" },
+  { src: image17, name: "Birla Cement" },
+];
+export default images;
